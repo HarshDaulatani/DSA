91d6b481b7ecf4f9a079da94794748e1dbf54d0b
Harsh Daulatani <harshdaulatani28@gmail.com>
2026-03-01 23:47:40 +0530

    88 Merge Sorted Array Solution

--- src/leetcode/RemoveDuplicateLinkedList.java
+++ src/leetcode/RemoveDuplicateLinkedList.java
@@ -1,16 +1,16 @@
 package leetcode;
 
 public class RemoveDuplicateLinkedList {
-    /**
-     * Definition for singly-linked list.
-     * public class ListNode {
-     *     int val;
-     *     ListNode next;
-     *     ListNode() {}
-     *     ListNode(int val) { this.val = val; }
-     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     * }
-     */
+
+     //* Definition for singly-linked list.
+     class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) {
+     val = x;
+     next = null;
+     }
+     }
     class Solution {
         public ListNode deleteDuplicates(ListNode head) {
             if(head == null){
