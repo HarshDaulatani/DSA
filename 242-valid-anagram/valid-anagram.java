0271b04896f1397c404edbc2b44570db73e1975b
Harsh Daulatani <147529205+HarshDaulatani@users.noreply.github.com>
2026-06-17 16:06:19 +0530

    Time: 4 ms (82.14%) | Memory: 46.7 MB (15.56%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char cs [] = s.toCharArray();
+        char ct [] = t.toCharArray();
+        Arrays.sort(cs);
+        Arrays.sort(ct);
+        if(Arrays.equals(cs,ct)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
