bea1f2971000b93a352bd482e8ae2f4de66f5629
Harsh Daulatani <147529205+HarshDaulatani@users.noreply.github.com>
2026-08-06 20:14:57 +0530

    Time: 2 ms (93.04%) | Memory: 43.1 MB (81.38%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        int b =0, a = 0, l =0, o = 0, n = 0;
+        for(char c : text.toCharArray()){
+            if(c == 'b') b++;
+            if(c == 'a') a++;
+            if(c == 'l') l++;
+            if(c == 'o') o++;
+            if(c == 'n') n++;
+        }
+
+        return Math.min(Math.min(b,a), Math.min(Math.min(l/2,o/2),n));
+        
+    }
+}
